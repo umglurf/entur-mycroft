b270d44b6b03ca0f873c132437589dfeb7f7776f
Håvard Moen <post@haavard.name>
2019-11-17 17:58:30 +0100

    change from yesno to get_response

--- __init__.py
+++ __init__.py
@@ -29,8 +29,8 @@ class Entur(MycroftSkill):
             'line': departure['line'], 
             'time': self._format_time(departure['departureTime'])
         }, wait=True)
-        yesno = self.ask_yesno("More departures?")
-        if yesno == 'yes':
+        response = self.get_response("more.departures")
+        if response == 'yes':
             for departure in departures[0:5]:
                 self.speak_dialog("next.departure", data={
                     'destination': departure['destination'],
